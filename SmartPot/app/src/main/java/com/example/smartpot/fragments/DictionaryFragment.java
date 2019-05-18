e66e3fec0ec1d16469a31e227d00ee42d48db041
KiseokYang <rltjr219@gmail.com>
2019-05-18 14:40:29 +0900

    0518/crawling completed

--- SmartPot/app/src/main/java/com/example/smartpot/fragments/DictionaryFragment.java
+++ SmartPot/app/src/main/java/com/example/smartpot/fragments/DictionaryFragment.java
@@ -18,7 +18,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.util.Dictionary;
 
 public class DictionaryFragment extends Fragment {
@@ -51,24 +50,10 @@ public class DictionaryFragment extends Fragment {
         }
     }
 
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//    }
-
-    TextView[] flowerDic = new TextView[4];
-
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dictionary, container, false);
-
-
-        flowerDic[0] = (TextView) view.findViewById(R.id.content);
-        flowerDic[1] = (TextView) view.findViewById(R.id.content1);
-        flowerDic[2] = (TextView) view.findViewById(R.id.content2);
-        flowerDic[3] = (TextView) view.findViewById(R.id.content3);
-
-        new DictionaryAsynTask(flowerDic).execute("https://terms.naver.com/list.nhn?cid=42526&categoryId=42527");
 
         return view;
     }
@@ -90,54 +75,4 @@ public class DictionaryFragment extends Fragment {
         void onFragmentInteraction(Uri uri);
     }
 
-    static class DictionaryAsynTask extends AsyncTask<String, Void, String[]>   {
-
-        private TextView[] textView;
-        private final String SELECTOR ="div[class = subject], p[class = desc __ellipsis is-truncated], div[class = related], div[class = related v2]  ";
-
-        public DictionaryAsynTask(TextView[] textView)  {
-            this.textView = textView;
-        }
-
-        @Override
-        protected String[] doInBackground(String... params) {
-
-            String URL = params[0];
-            String[] result = new String[4];
-
-            int count = 0;
-
-            try {
-                Document document = Jsoup.connect(URL).get();
-                Elements elements = document.select(SELECTOR);
-
-                for (Element element : elements) {
-                    if (element.text().trim().length() == 1)    {
-                        result[count] = "no";
-                    }
-                    else {
-                        if (count == 2)result[count] = element.text() + "\n";
-                        else if (count == 3) result[count] = element.text() + "\n";
-                        else result[count] = element.text() + "\n";
-                    }
-                    System.out.println(element.toString());
-                    count++;
-                }
-                return result;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-        @Override
-        protected void onPostExecute(String[] s) {
-            super.onPostExecute(s);
-
-            for (int i = 0; i < 4; i++) {
-                textView[i].setText(s[i]);
-            }
-
-        }
-    }
 }
